5f64106235c8594d442df9ae0332b94f3e5a51fd
Kevin <kevinrisqi22@gmail.com>
2020-04-09 02:12:44 +0700

    Initial Commit

---
+++ app/src/main/java/com/zenai/myviewandviews/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zenai.myviewandviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+    }
+}
